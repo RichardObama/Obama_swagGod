1036a5514622d547efb9f15ac0b320127c8c3f1a
RichardObama <113605267+RichardObama@users.noreply.github.com>
2022-10-05 15:37:45 +0200

    Obama

---
+++ falscherCode/main.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#define f=69
+/*
+ * 
+ */
+int a;
+float b=420;
+
+
+
+int     main(int argc, char** argv  ) 
+
+{
+        long long  bruh  ; //was läuft?
+     printf("Please Enter a number, and i will tell you if the number is even or odd 🤙🤙🤙 \n");
+      scanf("%d", &bruh);//hallo
+    if(bruh%2== 
+            //Ariq = Baum
+           
+            0){ printf   ("the number is even\n");}                                                                                                                                 //sus
+     else    {printf ("the number is odd\n");}//chill mal
+                                return 69;}
+
